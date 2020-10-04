f3f8dfb7ae59e9461205b25831d39bf051deddc6
T0mikCZ <oplpet95@gmail.com>
2020-10-04 23:06:14 +0200

    Worked on the useless Math library made by me

---
+++ Math.py
@@ -0,0 +1,91 @@
+listCisel = [13,2]
+
+def max(numberList):
+
+    maximum = numberList[0]
+
+    for i in range(len(numberList)):
+        if maximum > numberList[i]:
+            maximum = numberList[i]
+
+    return maximum
+
+def min(numberList):
+
+    minimum = numberList[0]
+
+    for i in range(len(numberList)):
+        if minimum < numberList[i]:
+            minimum = numberList[i]
+
+    return minimum
+
+def pow(number, powNumber):
+
+    return number ** powNumber
+
+
+def average(numberList):
+
+    average = 0
+
+    for i in range(len(numberList)):
+
+        average += numberList[i]
+
+    return average / len(numberList)
+
+def CalculateList(numberList, operator):
+    result = 0
+
+    if operator == "+":
+        for i in range(len(numberList)):
+            result += numberList[i]
+        return result
+    elif operator == "-":
+        for i in range(len(numberList)):
+            result = numberList[0]
+            result -= numberList[i]
+        return result
+    elif operator == "*":
+        for i in range(len(numberList)):
+            result = numberList[0]
+            result *= numberList[i]
+        return result
+    elif operator == "/":
+        for i in range(len(numberList)):
+            result = numberList[0]
+            result /= numberList[i]
+        return result
+    elif operator == "%":
+        for i in range(len(numberList)):
+            result = numberList[0]
+            result /= numberList[i]
+
+            rest = numberList[0]
+            rest %= numberList[i]
+        return int(result), rest
+    else:
+        return "Error: Wrong Operator"
+
+def floor(number):
+    return int(number)
+
+
+def calculate2Numbers(number1, number2, operator):
+
+    if operator == "+":
+        return number1 + number2
+    elif operator == "-":
+        return number1 - number2
+    elif operator == "*":
+        return number1 * number2
+    elif operator == "/":
+        return number1 / number2
+    elif operator == "%":
+        return int(number1 / number2), number1 % number2
+    else:
+        return "Error: Wrong Operator"
+
+print(calculate2Numbers(13,5,"%"))
+
