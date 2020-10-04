bcc9da16062a7700ba62b8d0667f0ef4535556a1
T0mikCZ <oplpet95@gmail.com>
2020-10-05 01:16:54 +0200

    config

--- TextGameNew.py
+++ TextGameNew.py
@@ -1,3 +1,23 @@
 import json
 
+configFile = open("config.json", "r")
+config = json.load(configFile)
+
+print(config["food"])
+print(config["drink"])
+print(config["health"])
+print(config["energy"])
+
+configFile.close
+
+
+
+
+#config["food"] = 0
+
+#configFile =  open("config.json", "w")
+#json.dump(config, configFile)
+
+#print(config["food"])
+#configFile.close()
 
