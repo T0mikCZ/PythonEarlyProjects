6b945bdb592ceed7a40ed2d3f16947b1e82e1dae
T0mikCZ <oplpet95@gmail.com>
2020-10-06 21:33:38 +0200

    Class Functions

--- ClassTest.py
+++ ClassTest.py
@@ -1,12 +1,16 @@
 from Classes import Student, Mobile
 
-student1 = Student("Tomas", "Programming", 3.2, False)
+student1 = Student("Tomas", "Programming", 3.6, False)
 student2 = Student("Michal", "Art", 2.2, True)
 
 redmi = Mobile("Redmi 7A", "MI11", 1.25, "11.0.4", "Blue" )
 
 print(student1.major)
 print(student2.major)
+
+print(f"{student1.name} has honor: {student1.onHonorRoll()}")
+print(f"{student2.name} has honor: {student2.onHonorRoll()}")
+
 print(f"Mobile name: {redmi.name}, Version: {redmi.OS} {redmi.version}, Colour: {redmi.colour}")
 
 name = input("Zadejte jmeno mobilu: ")
--- Classes.py
+++ Classes.py
@@ -4,6 +4,12 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.isOnProbation = isOnProbation
+        
+    def onHonorRoll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
 
 
 class Mobile:
