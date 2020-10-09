50aee5109e18551c6aa722a2fc3e64b31e2ec436
T0mikCZ <oplpet95@gmail.com>
2020-10-09 17:17:45 +0200

    Some work on the project

--- GuessWhat/GuessWhat.py
+++ GuessWhat/GuessWhat.py
@@ -9,4 +9,18 @@ bear = Animal("Bear", "Mammal", "Roar")
 animals = [wolf, dog, deer,  bear]
 randomAnimal = random.choice(animals)
 
-print(randomAnimal.name)
+
+print("Main Menu")
+print("1. Guess Animals")
+
+mainChoice = input("Choose the option: ")
+
+if mainChoice == "1":
+    print("You are guessing an animal")
+    print(f"The animal is a {randomAnimal.animalClass}")
+    print(f"The animal is making a {randomAnimal.animalSound} sound")
+
+    guessChoice = input("Guess the animal: ")
+
+    if guessChoice == randomAnimal.name:
+        print("You have guessed the animal")
