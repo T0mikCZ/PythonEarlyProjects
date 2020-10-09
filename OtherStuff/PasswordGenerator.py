fe08d3e5fa2cce09f10dec16e0fa9a87e589a8b0
T0mikCZ <oplpet95@gmail.com>
2020-10-09 12:18:27 +0200

    Made the password generator better

--- OtherStuff/PasswordGenerator.py
+++ OtherStuff/PasswordGenerator.py
@@ -3,7 +3,8 @@ import colorama
 
 colorama.init(strip=False)
 
-def generatePassword(password):
+def makePasswordStronger(password):
+
     passLog = open("PasswordLog.txt", "a")
 
     password = str(password)
@@ -37,12 +38,60 @@ def generatePassword(password):
 
     return finalPassword
 
-print("Password Generator!\n")
+def generatePassword(lengthOfPassword):
 
-inputPassword = input("Type a word you want to strength up: ")
+    passLog = open("PasswordLog.txt","a")
 
-print(f"Your generated password: {colorama.Fore.CYAN}{generatePassword(inputPassword)}\n{colorama.Style.RESET_ALL}")
-print("Your password has been save to a file: PasswordLog.txt")
+    password = ""
+
+    for _ in range(0,lengthOfPassword):
+
+        randomSymbols = random.choice(["!","@","#","$","%","&","_",":",":","'","/","~"])
+        randomLetters = random.choice(["A","B","C","D","E","Z","W","B","Y","H","S","O"])
+        randomNumber = random.randint(0,9)
+        randomChoice = random.randint(1,4)
+
+        if randomChoice == 1:
+            password += randomLetters
+        elif randomChoice == 2:
+            password += randomSymbols
+        elif randomChoice == 3:
+            password += str(randomNumber)
+        else:
+            password += randomLetters
+            
+    passLog.write(f"Password: {password}\n")
+    passLog.close()
+
+    return password
+
+
+
+print("Main Menu")
+print("1. Generate password from a word")
+print("2. Random generated password")
+
+passwordMenuChoice = input("Choose generating type: ")
+
+if passwordMenuChoice == "1":
+
+    print("Password Generator from a word!\n")
+
+    inputPassword = input("Type a word you want to strength up: ")
+
+    print(f"Your generated password: {colorama.Fore.CYAN}{makePasswordStronger(inputPassword)}\n{colorama.Style.RESET_ALL}")
+    print("Your password has been save to a file: PasswordLog.txt")
+
+elif passwordMenuChoice == "2":
+    print("Random password generator!\n")
+
+    passwordLength = int(input("Type a length of the password: "))
+
+    print(f"Your generated password: {colorama.Fore.CYAN}{generatePassword(passwordLength)}\n{colorama.Style.RESET_ALL}")
+    print("Your password has been save to a file: PasswordLog.txt")
+else:
+    print("Choice selected wrongly! You can only select between 1 and 2")
+
 
 input()
 exit()
