4256a9e2611a3abf635bd1fa93679d436133f120
T0mikCZ <oplpet95@gmail.com>
2020-10-11 22:47:13 +0200

    Fixed some logic and added energy to jobs

--- Games/JobClass.py
+++ Games/JobClass.py
@@ -1,8 +1,9 @@
 class Job:
 
-    def __init__(self, jobName, jobSalary):
+    def __init__(self, jobName, jobSalary, jobEnergy):
         self.jobName = jobName
         self.jobSalary = jobSalary
+        self.jobEnergy = jobEnergy
 
     def promote(self):
         self.jobSalary += int((self.jobSalary / 2)) 
--- Games/TextGame.py
+++ Games/TextGame.py
@@ -3,24 +3,35 @@ import json
 import time
 from JobClass import Job
 
-#def chooseJob(job, jobIndex, )
+#def chooseJob(job, jobIndex)
 
+def findJob(jobList, jobIndex):
+    currentjob = jobList[jobIndex]
+    print(f"You have succesfully got a job as a {currentjob.jobName}")
 
+    config["hasJob"] = True
+    config["currentJob"] = currentjob.jobName
+    config["jobIndex"] = jobIndex
+    configFile =  open("config.json", "w")
+
+    json.dump(config, configFile)
+
+    configFile.close()
 #Variables
 mainChoice = "yes"
 
 #Create job list
 jobList = []
 #Add Jobs to list
-jobList.append(Job("McDonald's Cook", 25))
-jobList.append(Job("Programmer", 40))
-jobList.append(Job("Animator", 50))
-jobList.append(Job("Construction Worker", 15))
-jobList.append(Job("Cook", 35))
-jobList.append(Job("Cook in Luxury Restaurant", 45))
-jobList.append(Job("Animator", 50))
-jobList.append(Job("Cashier", 20))
-jobList.append(Job("Politician", 75))
+jobList.append(Job("McDonald's Cook", 25, 5))
+jobList.append(Job("Programmer", 40, 10))
+jobList.append(Job("Animator", 50, 15))
+jobList.append(Job("Construction Worker", 15, 25))
+jobList.append(Job("Cook", 35, 20))
+jobList.append(Job("Cook in Luxury Restaurant", 45, 25))
+jobList.append(Job("Animator", 50, 20))
+jobList.append(Job("Cashier", 20, 5))
+jobList.append(Job("Politician", 75, 15))
 #End of jobs
 
 jsonFile = open("config.json", "r")
@@ -47,10 +58,12 @@ while mainChoice == "yes" or mainChoice == "yes".capitalize:
     print("Energy: " + str(config["energy"]))
     print("Money: " + str(config["money"]) + "$" +  "\n")
 
+    energy = config["energy"]
+
     jsonFile.close()
 
     if hasJob:
-        print("1. Got to work")
+        print("1. Go to work")
     else:
         print("1. Find a job")
     print("2. Go to store")
@@ -66,144 +79,73 @@ while mainChoice == "yes" or mainChoice == "yes".capitalize:
             jobChoice = input("\nChoose a job you want to work at: ")
 
             if jobChoice == "1":
-                currentjob = jobList[0]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 0
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 0)
 
             elif jobChoice == "2":
-                currentjob = jobList[1]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 1
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 1)
 
             elif jobChoice == "3":
-                currentjob = jobList[2]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 2
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 2)
 
             elif jobChoice == "4":
-                currentjob = jobList[3]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 3
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 3)
 
             elif jobChoice == "5":
-                currentjob = jobList[4]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 4
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 4)
                 
             elif jobChoice == "6":
-                currentjob = jobList[5]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 5
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()    
+                findJob(jobList, 5)
 
             elif jobChoice == "7":
-                currentjob = jobList[6]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 6
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()                                           
+                findJob(jobList, 6)                                         
 
             elif jobChoice == "8":
-                currentjob = jobList[7]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 7
-                configFile =  open("config.json", "w")
-
-                json.dump(config, configFile)
-
-                configFile.close()
+                findJob(jobList, 7)
 
             elif jobChoice == "9":
-                currentjob = jobList[8]
-                print(f"You have succesfully got a job as a {currentjob.jobName}")
 
-                config["hasJob"] = True
-                config["currentJob"] = currentjob.jobName
-                config["jobIndex"] = 8
-                configFile =  open("config.json", "w")
+                findJob(jobList, 8)
 
-                json.dump(config, configFile)
-
-                configFile.close()
             else:
                 print("The selected choice is wrong!")
         elif hasJob:
-            def work(job, hoursToWork):
+            def work(jobList, hoursToWork):
+                energy = config["energy"]
+
+                print(f"You have {energy} energy!")
 
                 if hoursToWork < 8 and hoursToWork > 0:
+                    job = jobList[jobIndex]
+                    if energy < 0 or (job.jobEnergy * hoursToWork) > energy:
 
-                    job = Job(currentConfigJob, job.jobSalary)
-                    print(f"You are employed as a {job.jobName}")
+                        print("You don't have enough energy to work this many hours!")
 
-                    input("If you want to work, then press any key: ")
+                    else:
 
-                    print("You work...")
-                    time.sleep(hoursToWork)
+                        print(f"You are employed as a {job.jobName}")
 
-                    print(f"You have worked for {hoursToWork} hours")
-                    print(f"You have earned {job.jobSalary * hoursToWork}$")
+                        input("If you want to work, then press any key: ")
 
-                    config["money"] += job.jobSalary * hoursToWork
-                    configFile =  open("config.json", "w")
-                    json.dump(config, configFile)
+                        print("You work...")
+                        time.sleep(hoursToWork)
 
-                    configFile.close()      
+                        print(f"You have worked for {hoursToWork} hours")
+                        print(f"You have earned {job.jobSalary * hoursToWork}$")
+
+                        config["money"] += job.jobSalary * hoursToWork
+                        config["energy"] -= job.jobEnergy * hoursToWork
+                        configFile =  open("config.json", "w")
+                        json.dump(config, configFile)
+
+                        configFile.close()
 
                 else:
                     print("The number of hours has to be less than 8 and more than 0 hours you slaver")
@@ -212,7 +154,7 @@ while mainChoice == "yes" or mainChoice == "yes".capitalize:
             while workChoice == "yes" or workChoice == "yes".capitalize:
                 hoursToWork = int(input("How many hours do you want to work: "))
 
-                work(currentjob, hoursToWork)
+                work(jobList, hoursToWork)
 
                 workChoice = input("Do you want to work more?: ")
     mainChoice = input("Do you want to return to main menu?: ")
