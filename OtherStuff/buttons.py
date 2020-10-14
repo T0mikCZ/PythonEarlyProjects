2e9e015dbea54979f364cffabfa5e60c6a4986ee
T0mikCZ <oplpet95@gmail.com>
2020-10-14 18:27:33 +0200

    adjusted entries

--- OtherStuff/buttons.py
+++ OtherStuff/buttons.py
@@ -5,8 +5,12 @@ def calculateOnClick(x,y):
     myLabel = tk.Label(root, text=f"{x}+{y} = {x+y}", fg="red")
     myLabel.pack()
 
-def showNameOnClick(name):
-    nameLabel = tk.Label(root, text=f"Your name is {name}")
+nameEntry = tk.Entry(root)
+nameEntry.grid(column=0, row=0)
+nameEntry.insert(0, "Enter your name: ")
+
+def showNameOnClick():
+    nameLabel = tk.Label(root, text=f"Your name is {nameEntry.get()}")
     nameLabel.grid(column=0, row=4)
 
 myInputBoxX = tk.Entry(root)
@@ -15,16 +19,10 @@ myInputBoxX = tk.Entry(root)
 myInputBoxY = tk.Entry(root)
 #myInputBoxY.pack()
 
-myNameLabel = tk.Label(root, text="Enter Your name below")
-myNameLabel.grid(column=0, row=0)
-
-nameEntry = tk.Entry(root)
-nameEntry.grid(column=0, row=1)
-
 myButton = tk.Button(root, text="Click to calculate", padx=5, pady=5, command= lambda: calculateOnClick(int(myInputBoxX.get()),int(myInputBoxY.get())), fg="blue", bg="black")
 #myButton.pack()
 
-nameButton = tk.Button(root,text="Click me for showing your name", command= lambda: showNameOnClick(nameEntry.get()))
-nameButton.grid(column=0, row=2)
+nameButton = tk.Button(root,text="Click me for showing your name", command=showNameOnClick)
+nameButton.grid(column=0, row=1)
 
 root.mainloop()
