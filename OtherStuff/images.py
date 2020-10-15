57e7876b6bf64b991aab93a16d55494f965304b5
T0mikCZ <oplpet95@gmail.com>
2020-10-15 20:47:07 +0200

    ImageViewer

--- OtherStuff/images.py
+++ OtherStuff/images.py
@@ -16,25 +16,71 @@ sharinganMS = ImageTk.PhotoImage(Image.open("C:/python/OtherStuff/Images/MadaraM
 sharinganEMS = ImageTk.PhotoImage(Image.open("C:/python/OtherStuff/Images/MadaraEternalMangekyo.png"))
 
 rinnegan = ImageTk.PhotoImage(Image.open("C:/python/OtherStuff/Images/Rinnegan.png"))
+
+imageList = [sharingan1, sharingan2, sharingan3, sharinganMS, sharinganEMS, rinnegan]
+
 #Defining image labels
 
-mySharingan1Label = tk.Label(image=sharingan1)
-mySharingan2Label = tk.Label(image=sharingan2)
-mySharingan3Label = tk.Label(image=sharingan3)
+myImageLabel = tk.Label(image=sharingan1)
+#mySharingan2Label = tk.Label(image=sharingan2)
+#mySharingan3Label = tk.Label(image=sharingan3)
 
-mySharinganMSLabel = tk.Label(image=sharinganMS)
-mySharinganEMSLabel = tk.Label(image=sharinganEMS)
+#mySharinganMSLabel = tk.Label(image=sharinganMS)
+#mySharinganEMSLabel = tk.Label(image=sharinganEMS)
 
-myRinneganLabel = tk.Label(image=rinnegan)
+#myRinneganLabel = tk.Label(image=rinnegan)
 #Displaying them
 
-mySharingan1Label.grid(row=0,column=0)
-mySharingan2Label.grid(row=0,column=1)
-mySharingan3Label.grid(row=0,column=2)
+myImageLabel.grid(row=0,column=0, columnspan=3)
+#mySharingan2Label.grid(row=0,column=1)
+#mySharingan3Label.grid(row=0,column=2)
 
-mySharinganMSLabel.grid(row=0,column=3)
-mySharinganEMSLabel.grid(row=0,column=4)
+#mySharinganMSLabel.grid(row=0,column=3)
+#mySharinganEMSLabel.grid(row=0,column=4)
 
-myRinneganLabel.grid(row=0, column=5)
+#myRinneganLabel.grid(row=0, column=5)
 
+def forward(imageIndex):
+    global myImageLabel
+    global buttonForward
+    global buttonBack
+
+    myImageLabel.grid_forget()
+    myImageLabel = tk.Label(image=imageList[imageIndex-1])
+
+    buttonForward = tk.Button(root, text=">>>", command=lambda: forward(imageIndex+1))
+    buttonBack = tk.Button(root, text="<<<", command= lambda: back(imageIndex-1))
+
+    if imageIndex == 6:
+       buttonForward = tk.Button(root, text=">>>", state=tk.DISABLED) 
+
+    myImageLabel.grid(row=0,column=0, columnspan=3)
+    buttonBack.grid(row=1, column=0)
+    buttonForward.grid(row=1, column=2) 
+
+def back(imageIndex):
+    global myImageLabel
+    global buttonForward
+    global buttonBack
+
+    myImageLabel.grid_forget()
+
+    myImageLabel = tk.Label(image=imageList[imageIndex-1])
+    buttonForward = tk.Button(root, text=">>>", command=lambda: forward(imageIndex+1))
+    buttonBack = tk.Button(root, text="<<<", command= lambda: back(imageIndex-1))
+
+    if imageIndex == 1:
+       buttonBack = tk.Button(root, text=">>>", state=tk.DISABLED) 
+
+    myImageLabel.grid(row=0,column=0, columnspan=3)
+    buttonBack.grid(row=1, column=0)
+    buttonForward.grid(row=1, column=2) 
+
+buttonBack = tk.Button(root, text="<<<", command=back, state=tk.DISABLED)
+buttonExit = tk.Button(root, text="Exit Program", command=root.quit)
+buttonForward = tk.Button(root, text=">>>", command = lambda: forward(2))
+
+buttonBack.grid(row=1, column=0)
+buttonExit.grid(row=1, column=1)
+buttonForward.grid(row=1, column=2)
 root.mainloop()
